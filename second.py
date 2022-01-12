e99facc24dee56bf70cd003b9ad688ac34aff502
Ayush Pandey <64723452+AyushPandey141@users.noreply.github.com>
2022-01-12 13:30:30 +0530

    Create second.py

---
+++ second.py
@@ -0,0 +1,11 @@
+def first():
+  a=[10,40,5,8,22,45,16]
+  print("Original List:",a)
+  a.sort()
+  print("After Sorting:",a)
+  smallest=a[0]
+  largest=a[-1]
+  print("Smallest Number is:",smallest)
+  print("Largest Number is:",largest)
+  
+ob=first()
